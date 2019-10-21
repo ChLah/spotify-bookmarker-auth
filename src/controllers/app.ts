d5d65ea5ecfd449e9d4e8782be18f3045ceb198e
Christoph Lahner <christoph.lahner@cimdata-sw.de>
2019-10-21 12:03:25 +0200

    Use https

--- src/controllers/app.ts
+++ src/controllers/app.ts
@@ -3,13 +3,22 @@ import cookieParser from 'cookie-parser';
 import * as admin from 'firebase-admin';
 import { router as AuthorizeRouter } from '../routes/authorize';
 import firebaseAdminCredentials from '../configurations/firebase-admin.json';
+import certPaths from '../configurations/cert-paths.json';
+import https from 'https';
+import fs from 'fs';
 
 export class App {
+    private httpsServer: https.Server;
     private express: express.Express;
     private port: number;
 
     constructor() {
         this.express = express();
+        const credentials: https.ServerOptions = { 
+            key: fs.readFileSync(certPaths.key), 
+            cert:  fs.readFileSync(certPaths.cert), 
+        };
+        this.httpsServer = https.createServer(credentials, this.express);
         this.port = process.env.port ? parseInt(process.env.port) : 3000;
 
         this.initializeMiddleware();
@@ -18,7 +27,7 @@ export class App {
     }
 
     public run() {
-        this.express.listen(this.port, () => {
+        this.httpsServer.listen(this.port, () => {
             console.log(`Node running and listening on port ${this.port}`)
         });
     }
